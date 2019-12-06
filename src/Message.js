7c3611c0d802193f6dac85c0994debcebe58ba04
Barth Marsault <34370542+BarthMarsault@users.noreply.github.com>
2019-12-06 02:14:38 +0100

    Le super composant Message !

---
+++ src/Message.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+
+
+function Message(props){
+
+    
+    
+    return(
+        <div>
+            {props.author} :<br/>
+            {props.text}   <br/>
+            {today}
+        </div>
+    );
+
+}
+
+export default Message
